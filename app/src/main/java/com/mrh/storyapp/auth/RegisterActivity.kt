48a3f9110acdcca893531a79b9876297246d520e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-05 18:06:24 +0700

    initial commit

---
+++ app/src/main/java/com/mrh/storyapp/auth/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.mrh.storyapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrh.storyapp.R
+import com.mrh.storyapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
