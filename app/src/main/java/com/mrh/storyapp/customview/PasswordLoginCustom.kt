b77ce4ce83936b8204b9e4df7d419f6afc17d0c8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-05 23:14:54 +0700

    create password validation on login form

---
+++ app/src/main/java/com/mrh/storyapp/customview/PasswordLoginCustom.kt
@@ -0,0 +1,53 @@
+package com.mrh.storyapp.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+import com.mrh.storyapp.R
+
+class PasswordLoginCustom : AppCompatEditText {
+    private lateinit var passwordCustomEditText: EditText
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Password"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+
+        passwordCustomEditText = findViewById(R.id.ed_login_password)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s!!.length < 8) {
+                    passwordCustomEditText.error = "Password harus lebih dari 8 karakter"
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+}
