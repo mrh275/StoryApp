779287c9aed7ad8ef47d3baa969f347b26cf1982
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-06 13:48:14 +0700

    create api services endpoint

---
+++ app/src/main/java/com/mrh/storyapp/api/ApiService.kt
@@ -0,0 +1,21 @@
+package com.mrh.storyapp.api
+
+import retrofit2.Call
+import  retrofit2.http.*
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("login")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<ResponseLogin>
+
+    @FormUrlEncoded
+    @POST("register")
+    fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<ResponseRegister>
+}
