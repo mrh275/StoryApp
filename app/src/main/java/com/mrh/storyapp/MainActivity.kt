2a4153b9358e80d0316ec393071ed3cf737844d8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-06 23:36:07 +0700

    fetch stored auth session data

--- app/src/main/java/com/mrh/storyapp/MainActivity.kt
+++ app/src/main/java/com/mrh/storyapp/MainActivity.kt
@@ -3,15 +3,52 @@ package com.mrh.storyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mrh.storyapp.auth.RegisterActivity
+import androidx.lifecycle.ViewModelProvider
+import com.mrh.storyapp.auth.LoginActivity
+import com.mrh.storyapp.data.UserModel
+import com.mrh.storyapp.data.UserPreference
 import com.mrh.storyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var userModel: UserModel
+    private lateinit var mUserPreference: UserPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        mUserPreference = UserPreference(this)
         setContentView(binding.root)
+
+        showPreferenceData()
+
+        binding.btnLogout.setOnClickListener{
+            mUserPreference.clearAuthSession()
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    private fun showPreferenceData() {
+        userModel = mUserPreference.getAuthSession()
+        binding.sessionToken.text = userModel.token
+        binding.userId.text = userModel.userId
+        binding.userName.text = userModel.name
+    }
+
+    private fun checkAuthSession() {
+        userModel = mUserPreference.getAuthSession()
+
+        if(userModel.token.isNullOrEmpty() && userModel.userId.isNullOrEmpty()) {
+            //Do nothing
+        } else {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
 }
