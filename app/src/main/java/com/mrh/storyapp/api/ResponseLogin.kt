7c8a72992278a5f3975d2f8fc1d3e4c9e54e2510
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-06 13:50:37 +0700

    create data class for receive login response

---
+++ app/src/main/java/com/mrh/storyapp/api/ResponseLogin.kt
@@ -0,0 +1,27 @@
+package com.mrh.storyapp.api
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseLogin(
+
+	@field:SerializedName("loginResult")
+	val loginResult: LoginResult,
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
+
+data class LoginResult(
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("userId")
+	val userId: String,
+
+	@field:SerializedName("token")
+	val token: String
+)
