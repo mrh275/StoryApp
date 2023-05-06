f6641e4781192e262b2a17a79c9765f40c36a8df
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-06 13:50:43 +0700

    create data class for receive register response

---
+++ app/src/main/java/com/mrh/storyapp/api/ResponseRegister.kt
@@ -0,0 +1,12 @@
+package com.mrh.storyapp.api
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseRegister(
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
