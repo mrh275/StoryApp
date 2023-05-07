8920b731f5b73febbb49dc07af8ff86eb99a2c9a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-07 14:36:23 +0700

    added method to request with auth token

--- app/src/main/java/com/mrh/storyapp/api/ApiConfig.kt
+++ app/src/main/java/com/mrh/storyapp/api/ApiConfig.kt
@@ -1,6 +1,8 @@
 package com.mrh.storyapp.api
 
 import com.mrh.storyapp.BuildConfig
+import com.mrh.storyapp.auth.OAuthInterceptor
+import com.mrh.storyapp.data.UserPreference
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,6 +17,26 @@ class ApiConfig {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
             }
             val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://story-api.dicoding.dev/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+        fun getApiWithToken(token: String): ApiService {
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(OAuthInterceptor(
+                    "Bearer",
+                    token
+                ))
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
