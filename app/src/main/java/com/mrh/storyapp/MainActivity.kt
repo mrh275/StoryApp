44116a43dba1e2f5db3459f734167586e57edd81
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-07 14:38:26 +0700

    create method to show stories with recycler view

--- app/src/main/java/com/mrh/storyapp/MainActivity.kt
+++ app/src/main/java/com/mrh/storyapp/MainActivity.kt
@@ -3,9 +3,13 @@ package com.mrh.storyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.mrh.storyapp.auth.LoginActivity
 import com.mrh.storyapp.data.UserModel
 import com.mrh.storyapp.data.UserPreference
+import com.mrh.storyapp.data.stories.ListStoryAdapter
+import com.mrh.storyapp.data.stories.ListStoryItem
+import com.mrh.storyapp.data.stories.StoryViewModel
 import com.mrh.storyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +17,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var userModel: UserModel
     private lateinit var mUserPreference: UserPreference
+    private lateinit var rvStories: RecyclerView
+    private lateinit var storyViewModel: StoryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         mUserPreference = UserPreference(this)
         setContentView(binding.root)
+        storyViewModel = StoryViewModel()
 
-        showPreferenceData()
+        rvStories = binding.rvStories
+        rvStories.setHasFixedSize(true)
+        storyViewModel.listStory.observe(this) { listStory ->
+            getAllStories(listStory)
+        }
 
         binding.btnLogout.setOnClickListener{
             mUserPreference.clearAuthSession()
@@ -30,10 +41,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showPreferenceData() {
-        userModel = mUserPreference.getAuthSession()
-        binding.sessionToken.text = userModel.token
-        binding.userId.text = userModel.userId
-        binding.userName.text = userModel.name
+    private fun getAllStories(listStoryItem: List<ListStoryItem>) {
+        val listStory = listStoryItem.map {
+            "${it.photoUrl}\n- ${it.name}"
+        }
+        val adapter = ListStoryAdapter(listStory)
+        binding.rvStories.adapter = adapter
     }
 }
