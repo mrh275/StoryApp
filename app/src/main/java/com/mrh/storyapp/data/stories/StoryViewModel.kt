2661f4cb4361f14b607e9e13bf10b69f467d0a07
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-10 14:49:20 +0700

    fixes some line and add method for detail story

--- app/src/main/java/com/mrh/storyapp/data/stories/StoryViewModel.kt
+++ app/src/main/java/com/mrh/storyapp/data/stories/StoryViewModel.kt
@@ -1,44 +1,68 @@
 package com.mrh.storyapp.data.stories
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mrh.storyapp.api.ApiConfig
-import com.mrh.storyapp.data.UserPreference
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class StoryViewModel : ViewModel() {
 
-    private val _listStory = MutableLiveData<List<ListStoryItem>>()
-    val listStory: LiveData<List<ListStoryItem>> = _listStory
+    private var listStory: MutableLiveData<List<ListStoryItem>> = MutableLiveData()
+    private var detailStory: MutableLiveData<ResponseDetailStory> = MutableLiveData()
 
     companion object {
         private const val TAG = "StoryViewModel"
     }
 
-    init {
-        findStories()
+    fun getListStoryObserve(): MutableLiveData<List<ListStoryItem>> {
+        return listStory
     }
 
-    private fun findStories() {
-        val client = ApiConfig.getApiWithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLUloTTdWM0tXU0VWc29mNW0iLCJpYXQiOjE2ODM0NDQ4MTl9.-iOOHW5gpujl7p-O5_gFSc30-EurMzcDTk9MwDQCmxs").getAllStories()
+    fun getDetailStoryObserve(): MutableLiveData<ResponseDetailStory> {
+        return detailStory
+    }
+    fun findStories() {
+        val client =
+            ApiConfig.getApiWithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLUloTTdWM0tXU0VWc29mNW0iLCJpYXQiOjE2ODM0NDQ4MTl9.-iOOHW5gpujl7p-O5_gFSc30-EurMzcDTk9MwDQCmxs")
+                .getAllStories()
         client.enqueue(object : Callback<ResponseStories> {
             override fun onResponse(
                 call: Call<ResponseStories>,
                 response: Response<ResponseStories>
             ) {
                 if(response.isSuccessful) {
-                    _listStory.value = response.body()?.listStory
+                    listStory.postValue(response.body()?.listStory)
                 } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
+                    Log.e(TAG, "onFailure : ${response.message()}")
                 }
             }
 
             override fun onFailure(call: Call<ResponseStories>, t: Throwable) {
-                Log.e(TAG, "onFailure: ${t.message}")
+                Log.e(TAG, "onFailure : ${t.message}")
+            }
+
+        })
+    }
+
+    fun setDetailStory(id: String) {
+        val client =
+            ApiConfig.getApiWithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLUloTTdWM0tXU0VWc29mNW0iLCJpYXQiOjE2ODM0NDQ4MTl9.-iOOHW5gpujl7p-O5_gFSc30-EurMzcDTk9MwDQCmxs")
+                .getDetailStory(id)
+        client.enqueue(object : Callback<ResponseDetailStory> {
+            override fun onResponse(
+                call: Call<ResponseDetailStory>,
+                response: Response<ResponseDetailStory>
+            ) {
+                if(response.isSuccessful) {
+                    detailStory.postValue(response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseDetailStory>, t: Throwable) {
+                Log.e(TAG, "onFailure : ${t.message}")
             }
 
         })
