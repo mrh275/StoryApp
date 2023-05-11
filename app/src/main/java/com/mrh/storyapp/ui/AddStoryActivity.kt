4fe2936becbda1f9a0a3bc7d4956c4d2cfd7c36f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-11 11:55:03 +0700

    create method to request permission to use camera

--- app/src/main/java/com/mrh/storyapp/ui/AddStoryActivity.kt
+++ app/src/main/java/com/mrh/storyapp/ui/AddStoryActivity.kt
@@ -1,22 +1,83 @@
 package com.mrh.storyapp.ui
 
+import android.Manifest
+import android.app.Application
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.mrh.storyapp.MainActivity
 import com.mrh.storyapp.R
 import com.mrh.storyapp.databinding.ActivityAddStoryBinding
+import com.mrh.storyapp.utils.Utils
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAddStoryBinding
+    private lateinit var currentPhotoPath: String
+    private lateinit var utils: Utils
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if(it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            myFile.let { file ->
+                utils.rotateFile(file)
+                binding.previewImg.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        utils = Utils()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Add New Story"
+
+        if(!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.btnCamera.setOnClickListener {
+            startTakePhoto()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_CODE_PERMISSIONS) {
+            if(!allPermissionGranted()) {
+                Toast.makeText(this@AddStoryActivity, "Tidak mendapatkan permission", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -33,5 +94,24 @@ class AddStoryActivity : AppCompatActivity() {
         super.onBackPressed()
         val intent = Intent(this@AddStoryActivity, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        utils.createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddStoryActivity,
+                "com.mrh.storyapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
 }
