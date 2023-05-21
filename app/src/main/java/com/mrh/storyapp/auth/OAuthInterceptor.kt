72b72b16cb37fc0d3986ad1bcd2bcd958fc97722
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-21 19:53:38 +0700

    simplified parameter

--- app/src/main/java/com/mrh/storyapp/auth/OAuthInterceptor.kt
+++ app/src/main/java/com/mrh/storyapp/auth/OAuthInterceptor.kt
@@ -4,12 +4,11 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class OAuthInterceptor constructor(
-    private val tokenType: String,
     private val accessToken: String
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        request = request.newBuilder().header("Authorization", "$tokenType $accessToken")
+        request = request.newBuilder().header("Authorization", "$accessToken")
             .build()
         return chain.proceed(request)
     }
