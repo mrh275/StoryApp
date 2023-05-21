b092b505ef3de5712034a4097790c18d0f2fba7a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-21 19:50:23 +0700

    simplified view model only for list story

---
+++ app/src/main/java/com/mrh/storyapp/ui/story/StoryViewModel.kt
@@ -0,0 +1,45 @@
+package com.mrh.storyapp.ui.story
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.*
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.mrh.storyapp.api.ApiConfig
+import com.mrh.storyapp.data.stories.ListStoryItem
+import com.mrh.storyapp.data.stories.ResponseDetailStory
+import com.mrh.storyapp.data.stories.StoryRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class StoryViewModel(storyRepository: StoryRepository) : ViewModel() {
+
+    val story: LiveData<PagingData<ListStoryItem>> =
+        storyRepository.getStory().cachedIn(viewModelScope)
+
+    private var detailStory: MutableLiveData<ResponseDetailStory> = MutableLiveData()
+    fun detailStory(id: String, context: Context) {
+        val client =
+            ApiConfig.getApiWithToken(context)
+                .getDetailStory(id)
+        client.enqueue(object : Callback<ResponseDetailStory> {
+            override fun onResponse(
+                call: Call<ResponseDetailStory>,
+                response: Response<ResponseDetailStory>
+            ) {
+                if (response.isSuccessful) {
+                    detailStory.postValue(response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseDetailStory>, t: Throwable) {
+                Log.e("StoryViewModel", "onFailure : ${t.message}")
+            }
+        })
+    }
+
+    fun getDetailStoryObserve(): LiveData<ResponseDetailStory> {
+        return detailStory
+    }
+}
