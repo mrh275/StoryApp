187173788e54084e8a44f502639d4cd3d79e49b1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-21 19:55:46 +0700

    create injection helper to passing parameter

---
+++ app/src/main/java/com/mrh/storyapp/di/Injection.kt
@@ -0,0 +1,14 @@
+package com.mrh.storyapp.di
+
+import android.content.Context
+import com.mrh.storyapp.api.ApiConfig
+import com.mrh.storyapp.data.stories.StoryRepository
+import com.mrh.storyapp.database.StoryDatabase
+
+object Injection {
+    fun provideRepository(context: Context) : StoryRepository {
+        val database = StoryDatabase.getDatabase(context)
+        val apiService = ApiConfig.getApiWithToken(context)
+        return StoryRepository(database, apiService)
+    }
+}
