488fe118af2b0606207d9473f2cd056722c1779e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-21 19:54:49 +0700

    refactor path and changed request method structure using repo

--- app/src/main/java/com/mrh/storyapp/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/mrh/storyapp/ui/auth/login/LoginActivity.kt
@@ -1,37 +1,28 @@
-package com.mrh.storyapp.auth
+package com.mrh.storyapp.ui.auth.login
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
-import com.mrh.storyapp.MainActivity
-import com.mrh.storyapp.api.ApiConfig
-import com.mrh.storyapp.api.ResponseLogin
-import com.mrh.storyapp.data.UserModel
-import com.mrh.storyapp.data.UserPreference
+import androidx.activity.viewModels
+import com.mrh.storyapp.ui.story.MainActivity
+import com.mrh.storyapp.ui.auth.register.RegisterActivity
+import com.mrh.storyapp.utils.UserPreference
 import com.mrh.storyapp.databinding.ActivityLoginBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.mrh.storyapp.utils.ViewModelFactory
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
     private lateinit var email: String
     private lateinit var password: String
-    private lateinit var token: String
-    private lateinit var userId: String
-    private lateinit var name: String
-    private lateinit var userModel: UserModel
     private var backPressedTime: Long = 0
-
-    companion object {
-        private const val TAG = "LoginActivity"
+    private val loginViewModel: LoginViewModel by viewModels {
+        ViewModelFactory(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +30,6 @@ class LoginActivity : AppCompatActivity() {
         checkSession()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        userModel = UserModel()
 
         binding.edLoginEmail.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -81,53 +71,38 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun authLogin(email: String, password: String) {
-        showLoading(true)
-        val client = ApiConfig.getApiService().login(email, password)
-        client.enqueue(object : Callback<ResponseLogin> {
-            override fun onResponse(call: Call<ResponseLogin>, response: Response<ResponseLogin>) {
-                if(response.isSuccessful) {
-                    showLoading(false)
-                    token = response.body()?.loginResult?.token.toString()
-                    userId = response.body()?.loginResult?.userId.toString()
-                    name = response.body()?.loginResult?.name.toString()
-                    storeAuthSession(token, userId, name)
-                    Toast.makeText(this@LoginActivity, "Welcome back ${response.body()?.loginResult?.name}", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    showLoading(false)
-                    Toast.makeText(this@LoginActivity, "Email atau Kata Sandi salah", Toast.LENGTH_SHORT).show()
-                    Log.e(TAG, "onFailure: ${response.message()}")
+        loginViewModel.login(email, password).observe(this) { result ->
+            if(result != null) {
+                when(result) {
+                    is com.mrh.storyapp.data.Result.Loading -> {
+                        showLoading(true)
+                    }
+                    is com.mrh.storyapp.data.Result.Success -> {
+                        showLoading(false)
+                        if(result.data.error) {
+                            Toast.makeText(this, result.data.message, Toast.LENGTH_LONG).show()
+                        } else {
+                            UserPreference.setAuthSession(result.data.loginResult.token, this)
+                            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                            finish()
+                        }
+                    }
+                    is com.mrh.storyapp.data.Result.Error -> {
+                        showLoading(false)
+                        Toast.makeText(this, result.error, Toast.LENGTH_LONG).show()
+                    }
                 }
             }
-
-            override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
-                showLoading(false)
-                Toast.makeText(this@LoginActivity, "Gagal terkoneksi ke server", Toast.LENGTH_SHORT).show()
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
+        }
     }
 
     private fun showLoading(isLoading: Boolean) {
         binding.progressBar.visibility = if(isLoading) View.VISIBLE else View.GONE
     }
 
-    private fun storeAuthSession(token: String, userId: String, name: String) {
-        val userPreference = UserPreference(this)
-
-        userModel.token = token
-        userModel.userId = userId
-        userModel.name = name
-
-        userPreference.setAuthSession(userModel)
-    }
-
     private fun checkSession() {
-        val userPreference = UserPreference(this)
-        val preferences = userPreference.getAuthSession()
-        if(!preferences.token.isNullOrEmpty()) {
+        val token = UserPreference.getAuthSession(this)
+        if(token.isNotEmpty()) {
             val intent = Intent(this@LoginActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
