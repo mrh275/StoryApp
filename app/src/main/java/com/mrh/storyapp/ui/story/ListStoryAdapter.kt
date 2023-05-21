b722eb3cc51a202453265cf98f6a5f81066ccd71
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-22 01:01:35 +0700

    re-enabled animtaion on cardview load

--- app/src/main/java/com/mrh/storyapp/ui/story/ListStoryAdapter.kt
+++ app/src/main/java/com/mrh/storyapp/ui/story/ListStoryAdapter.kt
@@ -37,10 +37,6 @@ class ListStoryAdapter : PagingDataAdapter<ListStoryItem, ListStoryAdapter.ListV
         this.onItemClickCallback = onItemClickCallback
     }
     class ListViewHolder(private val binding: ItemRowStoriesBinding) : ViewHolder(binding.root) {
-//        val owner: TextView = view.findViewById(R.id.tv_item_name)
-//        val imageUrl: ImageView = view.findViewById(R.id.iv_item_photo)
-//        val createdAt: TextView = view.findViewById(R.id.tv_created_at)
-//        val cvStory: CardView = view.findViewById(R.id.cv_stories)
         fun bind(data: ListStoryItem) {
             binding.tvItemName.text = data.name
             Glide.with(binding.ivItemPhoto)
@@ -48,49 +44,42 @@ class ListStoryAdapter : PagingDataAdapter<ListStoryItem, ListStoryAdapter.ListV
                 .into(binding.ivItemPhoto)
             binding.tvCreatedAt.text = data.createdAt
             binding.cvStories.startAnimation(AnimationUtils.loadAnimation(binding.cvStories.context, R.anim.grow_appear))
+            binding.root.setOnClickListener {
+
+            }
         }
     }
 
     interface OnItemClickCallback {
         fun onItemClicked(listStoryItem: ListStoryItem)
-
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ListViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row_stories, parent, false)
-//        return ListViewHolder(view)
         val binding = ItemRowStoriesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-//        holder.owner.text = liststory?.get(position)?.name
-//        holder.createdAt.text = liststory?.get(position)?.createdAt
-//        Glide.with(holder.imageUrl)
-//            .load(liststory?.get(position)?.photoUrl)
-//            .into(holder.imageUrl)
-//
-//        holder.cvStory.startAnimation(AnimationUtils.loadAnimation(holder.cvStory.context, R.anim.grow_appear))
         val data = getItem(position)
         if(data != null) {
             holder.bind(data)
         }
-        holder.itemView.setOnClickListener {
-            onItemClickCallback.onItemClicked(liststory?.get(holder.adapterPosition)!!)
-        }
+//        holder.itemView.setOnClickListener {
+//            onItemClickCallback.onItemClicked(liststory?.get(holder.adapterPosition)!!)
+//        }
     }
 
-    override fun getItemCount(): Int {
-        return if(liststory == null) 0
-        else liststory?.size!!
-    }
+//    override fun getItemCount(): Int {
+//        return if(liststory == null) 0
+//        else liststory?.size!!
+//    }
 
-    private var liststory: List<ListStoryItem>? = null
+//    private var liststory: List<ListStoryItem>? = null
 
-    fun setListStory(listStory: List<ListStoryItem>?) {
-        liststory = listStory
-    }
+//    fun setListStory(listStory: List<ListStoryItem>?) {
+//        liststory = listStory
+//    }
 }
