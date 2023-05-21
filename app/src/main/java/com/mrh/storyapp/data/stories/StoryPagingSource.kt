6b214963b799713582845da303d8358e9f5840c0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-21 19:51:39 +0700

    create paging source to request data and store in local database and serve using background process pagination

---
+++ app/src/main/java/com/mrh/storyapp/data/stories/StoryPagingSource.kt
@@ -0,0 +1,35 @@
+package com.mrh.storyapp.data.stories
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.mrh.storyapp.api.ApiService
+
+class StoryPagingSource(private val apiService: ApiService) : PagingSource<Int, ListStoryItem>() {
+
+    private companion object {
+        const val INITIAL_PAGE_INDEX = 1
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, ListStoryItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ListStoryItem> {
+        return try {
+            val page = params.key ?: INITIAL_PAGE_INDEX
+            val responseData = apiService.getAllStories(page, params.loadSize)
+
+            LoadResult.Page(
+                data = responseData.listStory,
+                prevKey = if(page == 1) null else page - 1,
+                nextKey = if(responseData.listStory.isEmpty()) null else page + 1
+            )
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+    }
+
+}
