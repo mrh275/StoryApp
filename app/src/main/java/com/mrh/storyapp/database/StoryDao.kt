4b03c085c5de2146ae6516fa5c9da8a4f981660f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-05-23 11:06:06 +0700

    create room dao/query for story

---
+++ app/src/main/java/com/mrh/storyapp/database/StoryDao.kt
@@ -0,0 +1,20 @@
+package com.mrh.storyapp.database
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mrh.storyapp.data.stories.ListStoryItem
+
+@Dao
+interface StoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStory(story: List<ListStoryItem>)
+
+    @Query("SELECT * FROM story")
+    fun getAllStory(): PagingSource<Int, ListStoryItem>
+
+    @Query("DELETE FROM story")
+    suspend fun deleteAll()
+}
